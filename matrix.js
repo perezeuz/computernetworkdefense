bd80d2403410519afeb68440db6c0920bff10409
Tom <112094539+perezeuz@users.noreply.github.com>
2024-10-18 23:14:35 +0200

    Update matrix.js

--- matrix.js
+++ matrix.js
@@ -1,28 +1,26 @@
-<script>
-    function generateMatrix() {
-        const container = document.createElement('div');
-        container.className = 'background';
-        document.body.appendChild(container);
+function generateMatrix() {
+    const container = document.createElement('div');
+    container.className = 'background';
+    document.body.appendChild(container);
 
-        let columns = Math.floor(window.innerWidth / 20);
-        for (let i = 0; i < columns; i++) {
-            let col = document.createElement('div');
-            col.className = 'column';
-            col.style.left = `${i * 20}px`;
-            col.style.animationDuration = `${Math.random() * 10 + 5}s`;
-            col.setAttribute('data-text', generateRandomText());
-            container.appendChild(col);
-        }
+    let columns = Math.floor(window.innerWidth / 20);
+    for (let i = 0; i < columns; i++) {
+        let col = document.createElement('div');
+        col.className = 'column';
+        col.style.left = `${i * 20}px`;
+        col.style.animationDuration = `${Math.random() * 10 + 5}s`;
+        col.setAttribute('data-text', generateRandomText());
+        container.appendChild(col);
     }
+}
 
-    function generateRandomText() {
-        const characters = 'abcdefghijklmnopqrstuvwxyz0123456789{}[]';
-        let result = '';
-        for (let i = 0; i < 100; i++) {
-            result += characters.charAt(Math.floor(Math.random() * characters.length));
-        }
-        return result;
+function generateRandomText() {
+    const characters = 'abcdefghijklmnopqrstuvwxyz0123456789{}[]';
+    let result = '';
+    for (let i = 0; i < 100; i++) {
+        result += characters.charAt(Math.floor(Math.random() * characters.length));
     }
+    return result;
+}
 
-    window.onload = generateMatrix;
-</script>
+window.onload = generateMatrix;
